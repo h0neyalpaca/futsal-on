5857715b40defa7fba5a8f9d0880ec284f87805b
minjeeleee <fly12035@gmail.com>
2021-09-26 03:35:50 +0900

    [이민지] modifyForm 오류 수정

--- WebContent/WEB-INF/views/mypage/modify-form.jsp
+++ WebContent/WEB-INF/views/mypage/modify-form.jsp
@@ -97,9 +97,9 @@
 							<tr>
 								<th>실력</th>
 								<td>
-									<label><input type="radio" name="capacity" id="capacity" value="상" ${authentication.capacity eq "상 "?"checked":""}/> 상</label>
-									<label><input type="radio" name="capacity" id="capacity" value="중" ${authentication.capacity eq "중 "?"checked":""}/> 중</label>
-									<label><input type="radio" name="capacity" id="capacity" value="하" ${authentication.capacity eq "하 "?"checked":""}/> 하</label>
+									<label><input type="radio" name="capacity" id="capacity1" value="상" ${authentication.capacity eq "상 "?"checked":""}/> 상</label>
+									<label><input type="radio" name="capacity" id="capacity2" value="중" ${authentication.capacity eq "중 "?"checked":""}/> 중</label>
+									<label><input type="radio" name="capacity" id="capacity3" value="하" ${authentication.capacity eq "하 "?"checked":""}/> 하</label>
 								</td>
 							</tr>
 							<tr>
@@ -112,11 +112,12 @@
 							</tr>
 						</table>
 					</form>
+					<script type="text/javascript" src="/resources/js/member/modifyForm.js"></script>
+					
 				</div>
 		</div>
 	</section>
 	
-<script type="text/javascript" src="/resources/js/member/modifyForm.js"></script>
 <%@ include file="/WEB-INF/views/include/footer.jsp" %>
 
 </body>
--- WebContent/resources/js/member/modifyForm.js
+++ WebContent/resources/js/member/modifyForm.js
@@ -2,8 +2,8 @@
  * 
  */
  (()=>{
-	  let confirmPw = "";
-	  let cofirmNick = "";
+	  let cofirmPw = "";
+	  let cofirmNick = document.querySelector('#nickName').value;
 
 	  document.querySelector('#btnPwCheck').addEventListener('click', function(){
 		  
@@ -24,7 +24,7 @@
 			})
 		   .then(text => {
 			   if(text == 'available'){
-				   confirmPw = password;
+				   cofirmPw = password;
 				   document.querySelector('#pwCheck').innerHTML = '비밀번호가 일치합니다';
 			   }else{
 				   document.querySelector('#pwCheck').innerHTML = '비밀번호가 일치하지 않습니다';
@@ -38,11 +38,23 @@
 	document.querySelector('#btnNickCheck').addEventListener('click', function(){
 		  
 		   let nickName = document.querySelector('#nickName').value;
-		   
+		   let nickReg = /^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,10}$/;
+
 		   if(!nickName){
 			   document.querySelector('#nickCheck').innerHTML = '닉네임을 입력하지 않았습니다';
 			   return;
 		   }
+		   
+		   if(nickName == cofirmNick){
+			   document.querySelector('#nickCheck').innerHTML = '원래 닉네임 입니다';
+			   return;
+		   }
+
+		   if(!nickReg.test(nickName)) {
+		   	   document.querySelector('#nickCheck').innerHTML = '닉네임은 4~10자로 설정해야합니다.';
+			   return;
+           }
+
 		   fetch("/mypage/nick-check?nickName="+nickName)
 		   .then(response =>{
 				if(response.ok){
@@ -65,19 +77,19 @@
 	   });
 	   
 	   document.querySelector('#frm_modify').addEventListener('submit',e=>{
-		   
-		   let nickName = document.querySelector('#nickName').value;
 		   let password = document.querySelector('#password').value;
-		  
-		   if(cofirmPw != password){
-			   document.querySelector('#nickCheck').innerHTML = '비밀번호 일치 검사를 하지 않습니다';
+		   let nickName = document.querySelector('#nickName').value;
+			console.dir(password);
+		   
+			if(cofirmNick != nickName){
+			   alert('닉네임 중복 검사를 하지 않았습니다');
 			   document.querySelector('#nickName').focus();
 			   e.preventDefault();
 		   }
 
-		   if(cofirmNick != nickName){
-			   document.querySelector('#nickCheck').innerHTML = '닉네임 중복 검사를 하지 않았습니다';
-			   document.querySelector('#nickName').focus();
+		   if(cofirmPw != password){
+			   alert('비밀번호 일치 검사를 하지 않습니다');
+			   document.querySelector('#password').focus();
 			   e.preventDefault();
 		   }
 	   })
