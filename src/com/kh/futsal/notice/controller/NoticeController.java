8dcfa70ada1bc7f518c6be29785232b01fde4b9b
shk1346 <shkk1346@naver.com>
2021-09-28 16:16:08 +0900

    [김수현] 조회수 중복방지 진행

--- src/com/kh/futsal/notice/controller/NoticeController.java
+++ src/com/kh/futsal/notice/controller/NoticeController.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.kh.futsal.member.model.dto.Member;
 import com.kh.futsal.notice.model.dto.Notice;
 import com.kh.futsal.notice.model.service.NoticeService;
 
@@ -71,8 +74,63 @@ public class NoticeController extends HttpServlet {
 		
 		String nwIdx = request.getParameter("noticeNo");
 		//System.out.println("nwIdx : " + nwIdx);
-		noticeService.noticeViewCnt(nwIdx); //조회수 업데이트
 		
+		//로그인되어있는 id 가져오기
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute("authentication");
+		System.out.println("member : " + member);
+	
+		//기존에 존재하던 쿠키 가져오기
+		Cookie[] cookies = request.getCookies();
+		
+		//null값 비교용 쿠키
+		Cookie nullCookie = null;
+		
+		//기존 쿠키 불러오기 (조회수 증가 X)
+		//로그인이 되있는 경우 userId와 nwIdx로 된 쿠키를 가져옴
+		if(cookies != null && cookies.length > 0 && member != null){ //cookie 존재하고 로그인되어있다면
+			for (int i = 0; i < cookies.length; i++) {
+				System.out.println("로그인 된 cookies[i].getName() : "+cookies[i].getName()); //cookieleader1001
+				if(cookies[i].getName().equals("cookie" + member.getUserId() + nwIdx)){ //cookie가 현재 로그인되어있는 id, 클릭한 nwIdx와 일치하다면
+					nullCookie = cookies[i]; 
+				}
+			}
+		}
+			
+		//로그인 되지 않았다면 게시글 번호로만 된 쿠키 가져오기
+		if(cookies != null && cookies.length > 0 && member == null){ //cookie가 있고 로그인되지 않았다면
+			for (int i = 0; i < cookies.length; i++) {
+				System.out.println("로그인되지 않은 cookies[i].getName() : " + cookies[i].getName()); //cookie1000
+				
+				if(cookies[i].getName().equals("cookie" + nwIdx)){ //클릭한 nwIdx와 같은 nwIdx를 가진 cookie가 존재한다면 
+					nullCookie = cookies[i]; //nullCookie를 cookie+nwIdx로 채운다.
+					System.out.println("nullCookie : " + nullCookie.getName());
+					
+				}
+			}
+		}
+			
+			
+		//cookie 생성
+		//로그인이 되있는 경우 userId와 nwIdx로 된 쿠키를 만든다.
+		if(member != null && nullCookie == null){
+			Cookie cookie = new Cookie("cookie"+ member.getUserId() + nwIdx, "cookie"+ member.getUserId() + nwIdx);
+			cookie.setMaxAge(60*60*12);
+			response.addCookie(cookie);
+			noticeService.noticeViewCnt(nwIdx); //조회수 업데이트
+		}
+		
+		//로그인 되지 않았다면 게시글 번호로만 된 쿠키를 만든다.
+		if(member == null && nullCookie == null){
+			Cookie cookie = new Cookie("cookie" + nwIdx, "cookie" + nwIdx);
+			cookie.setMaxAge(60*60*12); //12시간
+			response.addCookie(cookie);
+			noticeService.noticeViewCnt(nwIdx); //조회수 업데이트
+			//Set-Cookie: cookie1000=cookie1000; Max-Age=43200; Expires=Tue, 28-Sep-2021 15:27:50 GMT
+			//Set-Cookie: cookieleader1001=cookieleader1001; Max-Age=43200; Expires=Tue, 28-Sep-2021 15:29:56 GMT
+		} 
+	
+		//nwIdx에 따른 게시판 상세페이지	
 		noticeDetail = noticeService.selectNoticeDetail(nwIdx);
 		System.out.println("noticeDetail : " + noticeDetail);
 
@@ -141,10 +199,6 @@ public class NoticeController extends HttpServlet {
 		}
 		System.out.println("endPage : "+endPage);
 
-		
-		
-		
-		
 		List<Notice> noticeList = noticeService.selectNoticeList(startNo, endNo);
 		List<Notice> mainNoticeList = noticeService.selectMainNoticeList();
 		
